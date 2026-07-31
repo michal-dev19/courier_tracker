d8a6d2792b07d1502abc73bd1272bfba77981328
Michal <michaelsal1904@gmail.com>
2026-07-31 01:34:27 +0100

    added comments

--- database.py
+++ database.py
@@ -2,9 +2,13 @@ import sqlite3
 
 
 def get_db():
+    # we connect to our database and obtain the cursor to navigate it
     conn = sqlite3.connect("courier_tracker.db")
     cursor = conn.cursor()
+    # try block to yield conn and cursor (operations freeze after yield)
     try:
         yield conn, cursor
+    # once conn and cursor is no longer in use, or an error occurs at any point,
+    # the finally block ensures connection always closes
     finally:
         conn.close()
