d1ec6f1ba593f352a8de727bd940c79635e37421
Michal <michaelsal1904@gmail.com>
2026-08-01 02:00:38 +0100

    bug fixes, syntax refinement

--- auth.py
+++ auth.py
@@ -1,5 +1,8 @@
 import bcrypt
 import sqlite3
+import os
+import datetime as dt
+from dotenv import load_dotenv
 from jose import jwt, JWTError
 from pydantic import BaseModel
 from database import get_db
@@ -8,7 +11,10 @@ from fastapi import APIRouter, Depends, HTTPException, Header
 # use a router object to route current decorators/function to app in main
 router = APIRouter()
 
-SECRET_KEY = "F7D8S9B7389B7D9S039UV"
+# load local .env file
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 
 
@@ -25,17 +31,31 @@ def hash_password(password):
 
 # token creation upon successful login
 def create_token(id):
-    try:
-        return jwt.encode({"sub": id}, algorithm=ALGORITHM, key=SECRET_KEY)
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+    return jwt.encode(
+        {"sub": id, "exp": dt.datetime.now(dt.timezone.utc) + dt.timedelta(days=3)},
+        algorithm=ALGORITHM,
+        key=SECRET_KEY,
+    )
 
 
 # authorizes current user
-def get_current_user(authorization=Header()):
-    id = authorization.split(" ")[1]
+def get_current_user(authorization: str = Header(), conn_curs=Depends(get_db)):
+    conn, cursor = conn_curs
+    auth_split = authorization.split(" ")
+    # splits entire string into bearer variable and token string
+    if len(auth_split) == 2 and auth_split[0] == "Bearer":
+        bearer, token = auth_split
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    # we decode the given token, then check is user still exists in DB
     try:
-        return jwt.decode({"sub": id}, algorithms=ALGORITHM, key=SECRET_KEY)
+        payload = jwt.decode(token, algorithms=ALGORITHM, key=SECRET_KEY)
+        cursor.execute("SELECT id, email FROM users WHERE id=?", (payload["sub"],))
+        user = cursor.fetchone()
+        if user is not None:
+            return user
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized")
     except JWTError:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
@@ -57,7 +77,7 @@ def register_user(user_info: CreateUser, conn_curs=Depends(get_db)):
         return {"Status": "success"}
     except sqlite3.IntegrityError:
         conn.rollback()
-        raise HTTPException(status_code=400, detail="Bad Request")
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
 
 @router.post("/login")
@@ -68,23 +88,26 @@ def login_user(user_info: CreateUser, conn_curs=Depends(get_db)):
         cursor.execute(
             """
         SELECT
-        (id, email, password)
+        id, email, password
         FROM 
         users
-        WHERE email=(?)""",
+        WHERE email=?""",
             (user_info.email,),
         )
         credentials = cursor.fetchone()
-        if credentials is None:
-            raise HTTPException(status_code=400, detail="Bad Request")
-        if bcrypt.checkpw(
-            user_info.password.encode("utf-8"), credentials[2].encode("utf-8")
-        ):
-            return create_token(str(credentials[0]))
-        else:
-            raise HTTPException(status_code=401, detail="Unauthorized")
     except sqlite3.Error:
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    if credentials is None:
+        raise HTTPException(status_code=400, detail="Bad Request")
+    if bcrypt.checkpw(
+        user_info.password.encode("utf-8"), credentials[2].encode("utf-8")
+    ):
+        return {
+            "access_token": create_token(str(credentials[0])),
+            "token_type": "bearer",
+        }
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
 
 @router.get("/me")
---
+++ testtest.py
@@ -0,0 +1,3 @@
+import datetime as dt
+
+print(dt.timedelta(days=3))
